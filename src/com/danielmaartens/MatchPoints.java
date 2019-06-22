73be7f82544f71046313d73886cf19e8e4661f34
Daniel Maartens <daniel@touchfoundry.co.za>
2019-06-22 18:01:29 +0200

    Added MatchPoints class

---
+++ src/com/danielmaartens/MatchPoints.java
@@ -0,0 +1,23 @@
+package com.danielmaartens;
+
+public class MatchPoints {
+
+    private String team;
+    private Integer points;
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public String getTeam() {
+        return this.team;
+    }
+
+    public Integer getPoints() {
+        return this.points;
+    }
+}
